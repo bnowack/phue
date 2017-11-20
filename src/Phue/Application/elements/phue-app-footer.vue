9d9b685a8edc1a81460920cebb68692414cee9d6
Benjamin Nowack <mail@bnowack.de>
2017-11-20 16:11:30 +0100

    Implement initial `phue-app-footer` element (#appFooter)

---
+++ src/Phue/Application/elements/phue-app-footer.vue
@@ -0,0 +1,52 @@
+<template>
+    <footer>
+        <div>
+            powered by <a href="https://github.com/bnowack/phue">Phue</a>
+            <span class="version">v{{ version }}</span>
+        </div>
+    </footer>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return {
+                version: 0
+            }
+        },
+
+        mounted: function () {
+            this.version = window.phue.appVersion;
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '../scss/_variables.scss';
+
+    footer {
+        display: block;
+        width: 100%;
+        position: fixed;
+        bottom: 0;
+        line-height: $app-footer-height;
+        background: $app-footer-bg-color;
+        color: #666;
+        font-size: 14px;
+        text-align: right;
+        margin-top: 20px;
+    }
+
+    footer > div {
+        @include max-width-container();
+    }
+
+    a {
+        text-decoration: none;
+        color: #999;
+    }
+
+    .version {
+        font-size: 0.9em;
+    }
+</style>
