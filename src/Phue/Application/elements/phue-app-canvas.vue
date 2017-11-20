c234f598264feb31ad1d5f6f5c861ff94c8ae3cc
Benjamin Nowack <mail@bnowack.de>
2017-11-20 16:12:23 +0100

    Implement initial `phue-app-canvas` element (#appCanvas)

---
+++ src/Phue/Application/elements/phue-app-canvas.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="phue-app-canvas">
+        <slot></slot>
+    </section>
+</template>
+
+<script>
+
+    module.exports = {
+        data: function () {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '../scss/_variables.scss';
+
+    .phue-app-canvas {
+        @include max-width-container();
+    }
+</style>
+
