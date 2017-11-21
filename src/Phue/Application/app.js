b3da48714b0ce5590ce058f52a51d3dacf36a5b1
Benjamin Nowack <mail@bnowack.de>
2017-11-21 15:57:32 +0100

    Add initial `phue-app-content` element

--- src/Phue/Application/app.js
+++ src/Phue/Application/app.js
@@ -10,6 +10,7 @@ Vue.component('phue-app', () => import(/* webpackChunkName: "phue" */ './element
 Vue.component('phue-app-exception', () => import(/* webpackChunkName: "phue" */ './elements/phue-app-exception.vue'));
 Vue.component('phue-app-header', () => import(/* webpackChunkName: "phue" */ './elements/phue-app-header.vue'));
 Vue.component('phue-app-canvas', () => import(/* webpackChunkName: "phue" */ './elements/phue-app-canvas.vue'));
+Vue.component('phue-app-content', () => import(/* webpackChunkName: "phue" */ './elements/phue-app-content.vue'));
 Vue.component('phue-app-footer', () => import(/* webpackChunkName: "phue" */ './elements/phue-app-footer.vue'));
 
 new Vue({
---
+++ src/Phue/Application/elements/phue-app-content.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="phue-app-content">
+        <slot></slot>
+    </section>
+</template>
+
+<script>
+    module.exports = {
+        data() {
+            return {
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '../scss/_variables.scss';
+
+    .phue-app-content{
+    }
+</style>
+
