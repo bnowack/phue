81a279851135db7b189c91a62e05b3c72d735df4
Benjamin Nowack <mail@bnowack.de>
2017-11-25 14:01:18 +0100

    Implement dynamic routes that fetch partials from the server (#app)

--- src/Phue/Application/elements/phue-app-content.vue
+++ src/Phue/Application/elements/phue-app-content.vue
@@ -17,6 +17,12 @@
     @import '../scss/_variables.scss';
 
     .phue-app-content{
+        transition: opacity 0.5s;
+
+        &.outdated {
+            opacity: 0
+        }
+
         @include app-content();
     }
 </style>
--- src/Phue/Application/elements/phue-app.vue
+++ src/Phue/Application/elements/phue-app.vue
@@ -21,6 +21,7 @@
         mounted() {
             this.extendEnvironment();
             this.activateAxios();
+            this.activateRoutes();
             this.fadeIn();
         },
 
@@ -47,7 +48,54 @@
             },
 
             /**
-             * Sets the app container's opacity so that it appears
+             * Catches any route changes
+             */
+            activateRoutes() {
+                let router = window.phue.vue.$options.router;
+                router.afterEach(this.onRouteChange);
+            },
+
+            /**
+             * Fetches the view associated with a changed route
+             */
+            onRouteChange(to) {
+                // fade out content node
+                let $oldContent = document.querySelector('.phue-app-content');
+                $oldContent.classList.add('outdated');
+                // load new content
+                Vue.http.get(to.fullPath).then(this.renderContent);
+            },
+
+            /**
+             * Re-renders the content area with given response
+             */
+            renderContent(response) {
+                // update title
+                let $title = document.querySelector('title');
+                let title = response.headers['x-page-title'] || $title.innerText;
+                let titleSuffix = $title.getAttribute('data-suffix') || '';
+                $title.innerText = `${title}${titleSuffix}`;
+
+                // replace content area with dynamic component
+                let Component = Vue.extend({
+                    template: `
+                        <transition name="fade">
+                            <phue-app-content v-if="ready">${response.data}</phue-app-content>
+                        </transition>
+                    `,
+                    data() {
+                        return {
+                            ready: false
+                        }
+                    }
+                });
+                let component = new Component();
+                component.$mount(this.$el.querySelector('.phue-app-content'));
+                component.ready = true;// kick off transition
+            },
+
+            /**
+             * Sets the app container's opacity so that it appears on initial page load
              */
             fadeIn() {
                 document.querySelector('#app-container').style.opacity = 1;
