afb214e354ef7e275d779143bc6bcdc012245915
Benjamin Nowack <mail@bnowack.de>
2017-12-06 21:22:26 +0100

    Make it easier to (re-)use core app components (#app)

---
+++ src/Phue/Application/elements/phue-app.js
@@ -0,0 +1,110 @@
+import Vue from 'vue'
+import axios from 'axios'
+import qs from 'qs'
+
+export default {
+    data() {
+        return {
+        }
+    },
+
+    created() {
+    },
+
+    mounted() {
+        this.extendEnvironment();
+        this.activateAxios();
+        this.activateRoutes();
+        this.fadeIn();
+    },
+
+    methods: {
+        /**
+         * Adds forEach method to NodeLists and HTMLCollections (lacks in PhantomJS)
+         */
+        extendEnvironment() {
+            NodeList.prototype.forEach = Array.prototype.forEach;
+            HTMLCollection.prototype.forEach = Array.prototype.forEach;
+        },
+
+        /**
+         * Activates axios for API calls
+         */
+        activateAxios() {
+            phue.http = axios.create({
+                baseURL: phue.appBase,
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest',
+                    'X-Content-Only': 1
+                }
+            });
+            phue.http.postForm = function(url, data) {
+                return window.phue.http.post(
+                    url,
+                    qs.stringify(data),
+                    {
+                        headers: {
+                            'Content-Type': 'application/x-www-form-urlencoded'
+                        }
+                    }
+                )
+            }
+        },
+
+        /**
+         * Catches any route changes
+         */
+        activateRoutes() {
+            this.$router.afterEach(this.onRouteChange);
+        },
+
+        /**
+         * Fetches the view associated with a changed route
+         */
+        onRouteChange(to) {
+            // fade out content node
+            let $oldContent = document.querySelector('.phue-app-content');
+            $oldContent.classList.add('outdated');
+            // load new content
+            phue.http.get(to.fullPath).then(this.renderContent);
+        },
+
+        /**
+         * Re-renders the content area with given response
+         */
+        renderContent(response) {
+            // update title
+            let $title = document.querySelector('title');
+            let title = response.headers['x-page-title'] || $title.innerText;
+            let titleSuffix = $title.getAttribute('data-suffix') || '';
+            $title.innerText = `${title.replace(titleSuffix, '')}${titleSuffix}`;
+
+            // update appView
+            phue.appView = response.headers['x-app-view'];
+
+            // replace content area with dynamic component
+            let Component = Vue.extend({
+                template: `
+                    <transition name="fade">
+                        <phue-app-content v-if="ready">${response.data}</phue-app-content>
+                    </transition>
+                `,
+                data() {
+                    return {
+                        ready: false
+                    }
+                }
+            });
+            let component = new Component();
+            component.$mount(this.$el.querySelector('.phue-app-content'));
+            component.ready = true;// kick off transition
+        },
+
+        /**
+         * Sets the app container's opacity so that it appears on initial page load
+         */
+        fadeIn() {
+            document.querySelector('#app-container').style.opacity = 1;
+        }
+    }
+};
--- src/Phue/Application/elements/phue-app.vue
+++ src/Phue/Application/elements/phue-app.vue
@@ -5,129 +5,15 @@
     </div>
 </template>
 
-<script>
-    import Vue from 'vue'
-    import axios from 'axios'
-    import qs from 'qs'
-
-    export default {
-        data() {
-            return {
-            }
-        },
-
-        created() {
-        },
-
-        mounted() {
-            this.extendEnvironment();
-            this.activateAxios();
-            this.activateRoutes();
-            this.fadeIn();
-        },
-
-        methods: {
-            /**
-             * Adds forEach method to NodeLists and HTMLCollections (lacks in PhantomJS)
-             */
-            extendEnvironment() {
-                NodeList.prototype.forEach = Array.prototype.forEach;
-                HTMLCollection.prototype.forEach = Array.prototype.forEach;
-            },
-
-            /**
-             * Activates axios for API calls
-             */
-            activateAxios() {
-                window.phue.http = axios.create({
-                    baseURL: window.phue.appBase,
-                    headers: {
-                        'X-Requested-With': 'XMLHttpRequest',
-                        'X-Content-Only': 1
-                    }
-                });
-                window.phue.http.postForm = function(url, data) {
-                    return window.phue.http.post(
-                        url,
-                        qs.stringify(data),
-                        {
-                            headers: {
-                                'Content-Type': 'application/x-www-form-urlencoded'
-                            }
-                        }
-                    )
-                }
-            },
-
-            /**
-             * Catches any route changes
-             */
-            activateRoutes() {
-                this.$router.afterEach(this.onRouteChange);
-            },
-
-            /**
-             * Fetches the view associated with a changed route
-             */
-            onRouteChange(to) {
-                // fade out content node
-                let $oldContent = document.querySelector('.phue-app-content');
-                $oldContent.classList.add('outdated');
-                // load new content
-                phue.http.get(to.fullPath).then(this.renderContent);
-            },
-
-            /**
-             * Re-renders the content area with given response
-             */
-            renderContent(response) {
-                // update title
-                let $title = document.querySelector('title');
-                let title = response.headers['x-page-title'] || $title.innerText;
-                let titleSuffix = $title.getAttribute('data-suffix') || '';
-                $title.innerText = `${title.replace(titleSuffix, '')}${titleSuffix}`;
-
-                // update appView
-                phue.appView = response.headers['x-app-view'];
-
-                // replace content area with dynamic component
-                let Component = Vue.extend({
-                    template: `
-                        <transition name="fade">
-                            <phue-app-content v-if="ready">${response.data}</phue-app-content>
-                        </transition>
-                    `,
-                    data() {
-                        return {
-                            ready: false
-                        }
-                    }
-                });
-                let component = new Component();
-                component.$mount(this.$el.querySelector('.phue-app-content'));
-                component.ready = true;// kick off transition
-            },
-
-            /**
-             * Sets the app container's opacity so that it appears on initial page load
-             */
-            fadeIn() {
-                document.querySelector('#app-container').style.opacity = 1;
-            }
-        }
-    };
-</script>
+<script src="./phue-app.js"></script>
 
 <style src="../scss/app-theme.scss" lang="scss"></style>
-<style lang="scss" scoped>
+<style lang="scss">
     @import '../scss/_variables.scss';
 
     .phue-app {
-        display: block;
-        background-color: $app-bg-color;
-
+        box-sizing: border-box;
         min-height: 100vh;
         padding-bottom: $app-footer-height;
-        box-sizing: border-box;
     }
 </style>
